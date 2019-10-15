df8e4f2f35e6c8f5ad9d1dc05d7341454de95f77
Shubhanshu Ranjan <shubhanshu.ranjan@paytm.com>
2019-10-15 09:37:10 +0530

    Initial commit

---
+++ src/main/java/com/kafka/streamprocessing/StreamProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.streamprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamProcessingApplication.class, args);
+	}
+
+}
